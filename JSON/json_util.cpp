4050b21e9ce6a5688fd8790bf6f1a1a3b569316b
GagTad <136594120+GagTad@users.noreply.github.com>
2025-05-27 16:50:53 +0400

    Update json_util.cpp

--- JSON/json_util.cpp
+++ JSON/json_util.cpp
@@ -1,22 +1,8 @@
 #include "json.h"
-#include <iostream>
 
 using namespace json;
 
 json_data json_util::read(const char* filename) {
-	//std::ifstream file(filename);
-	//std::string ret = "";
-	//if (!file.is_open())
-	//{
-	//	std::cout << "ERRROOOOOOOOOOOOOOOOOOR" << std::endl;
-	//}
-	//else{
-	//	std::stringstream sstream;
-	//	sstream << file.rdbuf();
-	//	ret = sstream.str();
-	//}
-	//file.close();
-	
 	return json_data::parse(readFile(filename));
 }
 
